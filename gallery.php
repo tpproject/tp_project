cc67761d20869c6d449e06d986d9c832b1558d99
Boyan Penchev <boyan.penchev@gmail.com>
2012-05-07 22:44:15 +0300

    gallery(dont work)

---
+++ gallery.php
@@ -0,0 +1,111 @@
+﻿<?php
+session_start();
+	require("login.php");
+if (!isset($_SESSION['authuser'])) 
+     header('Location: http://localhost/tpproject/index.php');
+?>
+<!doctype html>  
+<html>  
+<head>  
+    <title>Картинки</title>  
+    <link rel="stylesheet" href="styles/style.css">
+    <meta charset="utf-8">
+</head>
+	<body>
+	<header>  
+        	<h1>Gaming Zone</h1>  
+    </header> 
+   	<nav>  
+        <ul>  
+            <li><a href="index.php">Начало</a></li>  
+    		<li><a href="reg.html">Регистрация</a></li> 
+            <li><a href="gallery.php">Картинки</a></li>    
+            <li><a href="luck.php">Късметчета</a></li>  
+			
+        </ul>     
+    </nav>  
+	<div id="content">
+    	<div id="mainContent">
+            <div class='img' id='img-1'> 
+            <div class='mask'></div>
+            <img src='images/1280x1024.jpg' width="200" height="200" />
+            </div>
+            
+            <div class='img' id='img-2'>
+            <div class='mask'></div>
+            <img src='images/alan_wake_44545-1280x1024.jpg' width="200" height="200" />
+            </div>
+        
+            <div class='img' id='img-3'>
+            <div class='mask' id='mask-1'></div>
+            <div class='mask' id='mask-2'></div>
+            <img src='images/ACB_1280x1024.jpg' width="200" height="200" />
+            </div>
+            
+            <div class='img' id='img-4'>
+            <div class='mask'></div>
+            <img src='images/Uplay_PC_Wallpaper4_1280x960.jpg' width="200" height="200" />
+            </div>
+            
+            <div class='img' id='img-5'>
+            <div class='mask'></div>
+            <img src='images/Uplay_PC_Wallpaper3_1280x960.jpg' width="200" height="200" />
+            </div>
+            
+            <div class='img' id='img-6'>
+            <div class='mask'></div>
+            <img src='images/Uplay_PC_Wallpaper2_1280x960.jpg' width="200" height="200" />
+            </div> 
+			
+			
+            <img src="images/luck.jpeg">
+             <div id="box">
+             	<center>
+                	<h3>Късметът ти днес:</h3>
+                    <p id="phrase"></p>
+                      <form>
+                <input type="button" id="luckButton" value="Изтегли">
+            </form> 
+                </center>
+                </div>   
+  
+           <div id="sidebar">
+        	<h3>
+            	<center>Класация</center>
+            </h3>
+        </div>   
+    	</div>
+	</div>
+    <footer>
+    	<div>
+            <section id="about">
+                <header>
+                    <h3>За сайта</h3>
+                </header>
+                <p>Сайта е разработен за проект по ТП през учебната 2011/2012 от ученици на 11б клас на ТУЕС.</p>
+            </section>
+
+            <section id="blogroll">
+                <header>
+                    <h3>Последвай ни</h3>
+                </header>
+                <ul>
+                    <li><a href="#">Facebook</a></li>
+                    <li><a href="#">Tweeter</a></li>
+                </ul>
+            </section>
+
+            <section id="popular">
+                <header>
+                    <h3>Популярни</h3>
+                </header>
+                <ul>
+                    <li><a href="#">Куизи</a></li>
+                    <li><a href="#">Изтегли Късметче</a></li>
+                </ul>
+            </section>
+        </div>
+    </footer>
+
+	</body>
+	</html>

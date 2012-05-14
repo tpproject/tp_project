84eee555513d3e6e17750b7634827472452cf78e
penchev <boyan.penchev@gmail.com>
2012-05-14 23:15:20 +0300

    login code

--- login.php
+++ login.php
@@ -1,13 +1,13 @@
 <?php 
+	
 	mysql_connect("localhost","root","wweraw") or die("Check your connection!");
 				
 	mysql_select_db("register");
 	$Username = $_POST['Username'];
 	$Password = $_POST['Password'];
-	
-	
 	session_start();
-	$_SESSION['Username'] = $_POST['Username'];
+	if(!isset($_SESSION['Username']))
+		$_SESSION['Username'] = $Username;
 	mysql_query("SELECT Username FROM users WHERE Username='". $Username ."' AND Password='". 	$Password ."'"); 
 	if(mysql_affected_rows()>0){
 		$_SESSION['authuser']=1;
